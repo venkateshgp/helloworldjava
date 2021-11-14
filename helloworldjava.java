a7993443403772b936d1ec1be6a81dcb45633895
venkateshgp <39702482+venkateshgp@users.noreply.github.com>
2021-11-14 22:30:49 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps! - Gedde le Naanu");
+    System.out.println("Hello World from DevOps! - Hogli le Naanu");
     }
 }
 
